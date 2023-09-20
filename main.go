25f14aa98d407be914ab17bb57ec0b8dc8782c27
Leonardo Viana <leonardoviana00@hotmail.com>
2023-09-19 21:24:46 -0300

    feat(monitor-server): Monitor básico HTTP

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
 
 func main() {
-	fmt.Println("Project Monitor Server - 'HTTP/HTTPS'")
+	agora := time.Now()
+	url := os.Args[1]
+	get, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Ocorreu um  erro ao executar o get(url)")
+		panic(err)
+	}
+	decorrido := time.Since(agora).Seconds()
+	status := get.StatusCode
+	fmt.Printf("Satus: [%d]\nTempo de carga: [%f]\n", status, decorrido)
 }
